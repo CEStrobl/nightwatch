c1e3591ff5d18a35c574e04fd84cbf260e359db5
cestrobl <charlotte@charlotteedencraft.com>
2024-12-31 11:47:44 -0500

    design progress

--- design/script.js
+++ design/script.js
@@ -1,1 +1,29 @@
-// setTimeout(function(){window.location.reload(1);}, 1000);
+let refreshInterval = null;
+
+function refresh(toggle) {
+    if (toggle === 'start') {
+        if (!localStorage.getItem('refreshLoop')) {
+            console.log("Starting refresh loop...");
+            localStorage.setItem('refreshLoop', 'true');
+            startRefreshing();
+        }
+    } else if (toggle === 'stop') {
+        console.log("Stopping refresh loop...");
+        localStorage.removeItem('refreshLoop');
+        clearInterval(refreshInterval);
+    } else {
+        console.log("Invalid toggle value. Use 'start' or 'stop'.");
+    }
+}
+
+function startRefreshing() {
+    refreshInterval = setInterval(() => {
+        console.log("Refreshing page...");
+        window.location.reload();
+    }, 1000);
+}
+
+// Automatically resume refresh loop if it was active
+if (localStorage.getItem('refreshLoop')) {
+    startRefreshing();
+}
