71c5da49eced254c7015a24cfa694361dcfab426
cestrobl <charlotte@charlotteedencraft.com>
2025-04-07 20:54:17 -0400

    move archive out of repo

--- archive/old/powershell/os.ps1
+++
@@ -1,5 +0,0 @@
-$IPAddress = "10.0.0.63"
-
-$PingResult = Test-Connection -ComputerName $IPAddress -Count 1
-$TTL = $PingResult | Select-Object -ExpandProperty ResponseTimeToLive
-Write-Output "TTL: $TTL (Potential OS)"
--- archive/storage.js
+++
@@ -1,4 +0,0 @@
-const pingOutput = document.getElementById('pingOutput');
-const comout = document.getElementById('comout');
-const comout2 = document.getElementById('comout2');
-
